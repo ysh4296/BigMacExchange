2fa950e548f2a5e2e9289e58dc788cc102eb92be
cadi <dbtmdgns4296@naver.com>
2024-12-21 19:13:05 +0900

    feat: add workflow for publish  eb803c

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     /* config options here */
     output: 'standalone',
+
+    webpack: (config) => {
+        console.log(config.resolve.alias);
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve('./src'),
+            '@/locales': path.resolve('./src/locales'),
+            '@/utils': path.resolve('./src/utils'),
+            '@/store': path.resolve('./src/store'),
+            '@/data': path.resolve('./src/data'),
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
