06c57d0e7323b05cc5c5818403ed54b108f3b359
cadi <dbtmdgns4296@naver.com>
2024-12-22 15:53:55 +0900

    feat: added metadata for seo optimize

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,20 @@
 import { Providers } from './providers';
+import { Metadata } from 'next';
 import '@radix-ui/themes/styles.css';
+import { getI18n } from '@/locales/server';
+
+export async function generateMetadata(): Promise<Metadata> {
+    const t = await getI18n();
+    return {
+        title: t('MainTitle'),
+        description: t('MainDescription'),
+        openGraph: {
+            url: 'https://bigmacexchange.com',
+            title: t('MainTitle'),
+            description: t('MainDescription'),
+        },
+    };
+}
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
