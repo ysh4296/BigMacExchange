1ebb2b16e3eac1329609cb7140d9554c35f99b9e
cadi <dbtmdgns4296@naver.com>
2024-12-24 00:06:18 +0900

    feat: [UI] add github link

--- web/src/app/[locale]/_components/header.tsx
+++ web/src/app/[locale]/_components/header.tsx
@@ -1,15 +1,31 @@
 'server-only';
 
-import { Text, Heading, Flex } from '@radix-ui/themes';
+import { Text, Heading, Flex, Link } from '@radix-ui/themes';
 import { getI18n } from '@/locales/server';
 import SelectLocale from '@/component/Input/selectLocale';
+import { FaGithub } from 'react-icons/fa';
 
 export default async function Header() {
     const t = await getI18n();
 
     return (
         <Flex as="div" direction={'column'} align={'center'} p={'6'} gap={'4'}>
-            <SelectLocale />
+            <Flex width={'100%'} height={'100%'}>
+                <Link
+                    href="https://github.com/ysh4296/BigMacExchange"
+                    style={{ alignItems: 'center' }}
+                >
+                    <FaGithub
+                        size={30}
+                        style={{
+                            height: '100%',
+                            justifySelf: 'center',
+                            alignSelf: 'center',
+                        }}
+                    />
+                </Link>
+                <SelectLocale />
+            </Flex>
             <Heading as="h1" color="indigo" size={{ initial: '8', sm: '9' }}>
                 {t('MainTitle')}
             </Heading>
